d7ca40b21833f372b5c91b8ebd104ac76799dc8b
hamzaezzzat <hamza2007ezzat@gmail.com>
2022-05-07 16:23:08 +0200

    commit

--- assets/js/portfolioData.js
+++ assets/js/portfolioData.js
@@ -1,26 +1,17 @@
 const Portfolios = [
     {
-        filterType: "filter-web filter-app",
-        imgTitle: "abaad",
-        imgP: "Web Application",
-        imgSrc: "portfolio-abaad.png",
-        portfolioLink: "https://abaad.news",
+        filterType: "filter-social",
+        imgTitle: "Kartonah",
+        imgP: "UI/UX Social Media",
+        imgSrc: "portfolio-kartonah.png",
+        portfolioLink: "https://www.behance.net/gallery/113803301/Kartonah",
     },
     {
-        filterType: "filter-branding filter-social",
-        imgTitle: "Alawadi",
-        imgP: "Branding & Social",
-        imgSrc: "Portfolio-alawadi1.png",
-        portfolioLink:
-            "https://www.behance.net/gallery/134706045/Mohamed-El-Awady",
-    },
-
-    {
-        filterType: "filter-web filter-branding filter-social",
-        imgTitle: "Alwadi",
-        imgP: "Web & Branding & Social",
-        imgSrc: "portfolio-alwadi.png",
-        portfolioLink: "https://alwadi-invest.com",
+        filterType: "filter-Branding",
+        imgTitle: "Kaaf",
+        imgP: "Branding",
+        imgSrc: "portfolio-kaaf.png",
+        portfolioLink: "https://www.behance.net/gallery/90755075/Kaaf-Enough",
     },
     {
         filterType: "filter-web",
@@ -30,19 +21,43 @@ const Portfolios = [
         portfolioLink: "http://attractantso.com",
     },
     {
-        filterType: "filter-web",
-        imgTitle: "ChauCyprus",
-        imgP: "Web application",
-        imgSrc: "portfolio-chau.png",
-        portfolioLink: "http://chaucyprus.attractantso.com",
-    },
-    {
         filterType: "filter-web filter-branding filter-social ",
         imgTitle: "ConnectedCode",
         imgP: "Web Site & Branding & Social",
         imgSrc: "portfolio-connected.png",
         portfolioLink: "http://connectedcode.co",
     },
+    {
+        filterType: "filter-web filter-branding filter-social",
+        imgTitle: "Alwadi",
+        imgP: "Web & Branding & Social",
+        imgSrc: "portfolio-alwadi.png",
+        portfolioLink: "https://alwadi-invest.com",
+    },
+    {
+        filterType: "filter-social",
+        imgTitle: "Pazarsepeti",
+        imgP: "UI/UX Social Media",
+        imgSrc: "portfolio-pazar.png",
+        portfolioLink: "https://www.behance.net/gallery/113803889/PazarSepeti",
+    },
+
+    {
+        filterType: "filter-web filter-app",
+        imgTitle: "abaad",
+        imgP: "Web Application",
+        imgSrc: "portfolio-abaad.png",
+        portfolioLink: "https://abaad.news",
+    },
+
+    {
+        filterType: "filter-web",
+        imgTitle: "ChauCyprus",
+        imgP: "Web application",
+        imgSrc: "portfolio-chau.png",
+        portfolioLink: "http://chaucyprus.attractantso.com",
+    },
+
     {
         filterType: "filter-github",
         imgTitle: "Facial Recognation",
@@ -57,6 +72,14 @@ const Portfolios = [
         imgSrc: "Portfolio-alawadi2.png",
         portfolioLink:
             "https://www.behance.net/gallery/134706405/Mohamed-El-Awady",
+    },
+    {
+        filterType: "filter-branding filter-social",
+        imgTitle: "Alawadi",
+        imgP: "Branding & Social",
+        imgSrc: "Portfolio-alawadi1.png",
+        portfolioLink:
+            "https://www.behance.net/gallery/134706045/Mohamed-El-Awady",
     },
     {
         filterType: "filter-social",
@@ -87,26 +110,5 @@ const Portfolios = [
         imgSrc: "portfolio-habib.png",
         portfolioLink:
             "https://www.behance.net/gallery/113802607/Habib-Diamond",
-    },
-    {
-        filterType: "filter-Branding",
-        imgTitle: "Kaaf",
-        imgP: "Branding",
-        imgSrc: "portfolio-kaaf.png",
-        portfolioLink: "https://www.behance.net/gallery/90755075/Kaaf-Enough",
-    },
-    {
-        filterType: "filter-social",
-        imgTitle: "Kartonah",
-        imgP: "UI/UX Social Media",
-        imgSrc: "portfolio-kartonah.png",
-        portfolioLink: "https://www.behance.net/gallery/113803301/Kartonah",
-    },
-    {
-        filterType: "filter-social",
-        imgTitle: "Pazarsepeti",
-        imgP: "UI/UX Social Media",
-        imgSrc: "portfolio-pazar.png",
-        portfolioLink: "https://www.behance.net/gallery/113803889/PazarSepeti",
     },
 ];
