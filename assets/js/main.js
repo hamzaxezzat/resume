b8c13c3cab1a4a48ef7f948826eaafe3d6cc37a0
hamzaezzzat <hamza2007ezzat@gmail.com>
2022-05-07 17:40:48 +0200

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -198,13 +198,13 @@ Portfolios.forEach((portfolio) => {
     const divPortfilioContent = `
   <div class="col-lg-4 col-md-6 portfolio-item ${portfolio.filterType}">
   <div class="portfolio-wrap">
-    <img src="assets/img/portfolio/${portfolio.imgSrc}" class="img-fluid" alt="${portfolio.imgTitle}">
+    <img src="http://www.hamzaezzat.com/assets/img/work/${portfolio.imgSrc}" class="img-fluid" alt="${portfolio.imgTitle}">
     <div class="portfolio-info">
       <h4>${portfolio.imgTitle}</h4>
       <p>${portfolio.imgP}</p>
       <div class="portfolio-links">
-        <a href="assets/img/portfolio/${portfolio.imgSrc}" data-gall="portfolioGallery" class="venobox" title="App 1"><i class="bx bx-plus"></i></a>
-        <a href="${portfolio.portfolioLink}" data-gall="portfolioDetailsGallery" data-vbtype="iframe" class="venobox" title="Portfolio Details"><i class="bx bx-link"></i></a>
+        <a href="http://www.hamzaezzat.com/assets/img/work/${portfolio.imgSrc}" data-gall="portfolioGallery" class="venobox" title="App 1"><i class="bx bx-plus"></i></a>
+        <a href="${portfolio.portfolioLink}" target="_blank"><i class="bx bx-link"></i></a>
       </div>
     </div>
   </div>
